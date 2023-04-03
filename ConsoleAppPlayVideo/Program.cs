9bc23206f84ee4447d8a4cf72f1b01726ae52334
YossyHal <yossyhal.k@gmail.com>
2023-04-03 13:45:47 +0900

    add Play Video src

--- ConsoleAppPlayVideo/Program.cs
+++ ConsoleAppPlayVideo/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OpenCvSharp;
+
+string movie = Path.Combine("movie", "bach.mp4");
+if (File.Exists(movie))
+{
+    Console.WriteLine("'" + movie + "' exists.");
+}
+else
+{
+    Console.WriteLine("'" + movie + "' does not exist.");
+    return;
+}
+
+using var capture = new VideoCapture(movie);
+if (!capture.IsOpened())
+{
+    Console.WriteLine("Could not load movie...");
+    return;
+}
+
+var image = new Mat();
+using var window = new Window("capture");
+int sleepTime = (int)Math.Round(1000 / capture.Fps);
+
+while (true)
+{
+    capture.Read(image); // same as cvQueryFrame
+    if (image.Empty())
+        break;
+    window.ShowImage(image);
+    int key = Cv2.WaitKey(sleepTime);
+    if (key == 27) break; // ESC キーで閉じる
+}
